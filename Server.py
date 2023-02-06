19881e729a6e03d6366c5ce10854f2c8986ae1d8
OmarKau <98516711+OmarKau@users.noreply.github.com>
2023-02-07 01:15:18 +0300

    making the code more readable

--- Server.py
+++ Server.py
@@ -52,6 +52,55 @@ allergies = {
 # url hosted in local device by node.js
 api_url = "https://d977-2a02-9b0-402b-d298-10c9-2e2f-13a9-243a.eu.ngrok.io/posts"
 
+def spliting_request(user_request):
+    # split requests based on key that came from user request
+    # the request contains an id,image, and all allergies
+    user_allergy = []
+    for key in user_request:
+        # if the key was id it will take the value and put in another variable
+        if key == "id":
+            user_id = user_request["id"]
+            user_allergy.append(user_id)
+        # if the key was image it will extract the base64 code and convert it into image
+        elif key == "image":
+            image_string = user_request["image"]
+            # turn the image from string into bytes
+            image_bytes = bytes(image_string, 'utf-8')
+            image_64_decode = base64.b64decode(image_bytes)
+            image_result = open('test.png', 'wb')
+            # create a writable image and write the decoding result
+            image_result.write(image_64_decode)
+        # if the above condition were not met, it will check if the allergy is true and put it into a list
+        else:
+
+            if user_request[key] == "true":
+                print(user_request[key])
+                user_allergy.append(key)
+
+    return user_allergy
+
+def text_classification(user_allergy,ocr_text):
+    # here it will start the classification function
+    # it will move through all the user allergies and check what causes that allergy
+    allergic_found = []
+    for allergen in user_allergy:
+        for causes in allergies[allergen]["causes"]:
+            # it will compare every word that came from the image and check if it matches the causes
+            if causes in ocr_text:
+                print(allergen)
+
+                # if it does it will put what allergy it was in a list
+                allergic_found.append(allergen + " / " + allergies[allergen]["arabic_allergy"])
+
+    return allergic_found
+
+
+def is_allergic(allergic_found):
+    allergic = False
+    if allergic_found:
+        allergic = True
+    return allergic
+
 
 def checkRequest(allergies):
     response = requests.get(api_url)
@@ -67,30 +116,12 @@ def checkRequest(allergies):
 
         print(tasks)
         print(user_request)
-        # split requests based on key that came from user request
-        # the request contains an id,image, and all allergies
-        user_allergy = []
-        for key in user_request:
-            # if the key was id it will take the value and put in another variable
-            if key == "id":
-                user_id = user_request["id"]
-            # if the key was image it will extract the base64 code and convert it into image
-            elif key == "image":
-                image_string = user_request["image"]
-                # turn the image from string into bytes
-                image_bytes = bytes(image_string, 'utf-8')
-                image_64_decode = base64.b64decode(image_bytes)
-                image_result = open('test.png', 'wb')
-                # create a writable image and write the decoding result
-                image_result.write(image_64_decode)
-            # if the above condition were not met, it will check if the allergy is true and put it into a list
-            else:
-
-                if user_request[key] == "true":
-                    print(user_request[key])
-                    user_allergy.append(key)
+        user_allergy= spliting_request(user_request)
+        user_id = user_allergy.pop(0)
+        print(user_allergy)
+        print(user_id)
         #  here it will start the OCR function
-        allergic_found = []
+
         # the line below needed for the OCR
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
         # it will extract img we decoded above to extract the text and put it into a list via split
@@ -98,17 +129,8 @@ def checkRequest(allergies):
 
         ocr_text = list(re.split(r"[-«»;ء؛,.\s]\s*", image))
         print(ocr_text)
-        # here it will start the classification function
-        # it will move through all the user allergies and check what causes that allergy
-        allergic = False
-        for allergen in user_allergy:
-            for causes in allergies[allergen]["causes"]:
-                # it will compare every word that came from the image and check if it matches the causes
-                if causes in ocr_text:
-                    print(allergen)
-                    allergic = True
-                    # if it does it will put what allergy it was in a list
-                    allergic_found.append(allergen +" / "+allergies[allergen]["arabic_allergy"])
+        allergic_found = text_classification(user_allergy,ocr_text)
+        allergic = is_allergic(allergic_found)
         print(f"allergic found {allergic_found}")
         # final step it will post the result back to the front-end
         todo = {"id": user_id, "allergies": allergic_found, "allergic": allergic}
--- test/delete.py
+++ test/delete.py
@@ -1,8 +1,22 @@
 import requests
 
-api_url = "https://36d1-31-166-28-3.in.ngrok.io/tasks"
+api_url = "https://d977-2a02-9b0-402b-d298-10c9-2e2f-13a9-243a.eu.ngrok.io/tasks"
 response = requests.get(api_url)
 tasks = response.json()
 while tasks:
     user_request = tasks.pop(0)
     requests.delete((api_url + f"/{user_request['id']}"))
+
+
+# def something():
+#     haha=[]
+#     haha.append("lol")
+#     haha.append(123)
+#     print(haha)
+#     return haha
+#
+# lol =something()
+# print(lol)
+# lmao =lol.pop(0)
+# print(lol)
+# print(lmao)
