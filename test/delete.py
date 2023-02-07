dd0339ebfb17165a3cf308546a08c5a4171a4bcb
OmarKau <98516711+OmarKau@users.noreply.github.com>
2023-02-07 17:28:44 +0300

    testing

--- test/delete.py
+++ test/delete.py
@@ -1,22 +1,16 @@
-import requests
-
-api_url = "https://d977-2a02-9b0-402b-d298-10c9-2e2f-13a9-243a.eu.ngrok.io/tasks"
-response = requests.get(api_url)
-tasks = response.json()
-while tasks:
-    user_request = tasks.pop(0)
-    requests.delete((api_url + f"/{user_request['id']}"))
-
-
-# def something():
-#     haha=[]
-#     haha.append("lol")
-#     haha.append(123)
-#     print(haha)
-#     return haha
+# import requests
 #
-# lol =something()
-# print(lol)
-# lmao =lol.pop(0)
-# print(lol)
-# print(lmao)
+# api_url = "https://8bdc-31-166-28-3.in.ngrok.io/posts"
+# response = requests.get(api_url)
+# tasks = response.json()
+# while tasks:
+#     user_request = tasks.pop(0)
+#     requests.delete((api_url + f"/{user_request['id']}"))
+from collections import Counter
+
+something = ["ياحبيبي","سبرايت","هلا"]
+something2 = ["هلا","ياحبيبي","سبرايت"]
+if Counter(something2) == Counter(something):
+    print("yesssssssssssssss")
+else:
+    print("nooooooooooooo")
